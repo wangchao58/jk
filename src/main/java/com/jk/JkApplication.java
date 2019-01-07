bf095587cf137e3dd449160e59990c7213c783fd
wangchao <657446308@qq.com>
2019-01-07 10:40:15 +0800

    首次提交

--- src/main/java/com/jk/JkApplication.java
+++ src/main/java/com/jk/JkApplication.java
@@ -2,10 +2,12 @@ package com.jk;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan("com.jk.mapper")
+@EnableAutoConfiguration
 @EnableScheduling
 @SpringBootApplication
 public class JkApplication {
