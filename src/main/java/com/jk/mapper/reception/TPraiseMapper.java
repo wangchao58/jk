d3853373dbbf890a66ae164f812d424751b74442
kaixuan1314 <1197827508@qq,com>
2019-01-15 16:27:55 +0800

    点赞数据表生成

---
+++ src/main/java/com/jk/mapper/reception/TPraiseMapper.java
@@ -0,0 +1,21 @@
+package com.jk.mapper.reception;
+
+import com.jk.entity.reception.TPraise;
+import com.jk.entity.reception.TPraiseExample;
+import java.util.List;
+
+public interface TPraiseMapper {
+    int deleteByPrimaryKey(String tId);
+
+    int insert(TPraise record);
+
+    int insertSelective(TPraise record);
+
+    List<TPraise> selectByExample(TPraiseExample example);
+
+    TPraise selectByPrimaryKey(String tId);
+
+    int updateByPrimaryKeySelective(TPraise record);
+
+    int updateByPrimaryKey(TPraise record);
+}
