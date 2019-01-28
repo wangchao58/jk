6903c4ca2435c85694408a4cdeb761ad86230ef0
wangchao <657446308@qq.com>
2019-01-28 23:32:37 +0800

    评论接口

--- src/main/java/com/jk/service/evaluate/impl/EvaluateServicrImpl.java
+++ src/main/java/com/jk/service/evaluate/impl/EvaluateServicrImpl.java
@@ -2,8 +2,10 @@ package com.jk.service.evaluate.impl;
 
 import com.jk.entity.reception.TEvaluate;
 import com.jk.entity.reception.TInformation;
+import com.jk.entity.reception.TStore;
 import com.jk.mapper.reception.TEvaluateMapper;
 import com.jk.mapper.reception.TInformationMapper;
+import com.jk.mapper.reception.TStoreMapper;
 import com.jk.service.evaluate.EvaluateServicr;
 import com.jk.util.DateUtil;
 import com.jk.util.UUIDUtil;
@@ -22,6 +24,8 @@ public class EvaluateServicrImpl implements EvaluateServicr {
     TEvaluateMapper tEvaluateMapper;
     @Autowired
     TInformationMapper tInformationMapper;
+    @Autowired
+    TStoreMapper tStoreMapper;
 
     /**
      * 资讯评论（接口）
@@ -31,20 +35,44 @@ public class EvaluateServicrImpl implements EvaluateServicr {
     @Override
     public int addEvaluate(TEvaluate tEvaluate) {
         int i = 0;
+        if(tEvaluate.gettType().equals("3")) {
+            i = getInformation(tEvaluate);
+        } else if(tEvaluate.gettType().equals("1")) {
+            i = gettStore(tEvaluate);
+        }
+
+        return i;
+    }
+
+    private int getInformation(TEvaluate tEvaluate) {
+        int i;
         String dateNow = DateUtil.formatDate(new Date(),"yyyy-MM-dd HH:mm");
         TInformation tInformation = new TInformation();
         tInformation.settId(tEvaluate.gettOtherId());
+
         TInformation informationByTid = tInformationMapper.selectByPrimaryKeyByprore(tEvaluate.gettOtherId());
+        tEvaluate.setfId(informationByTid.getpId());
 
         tEvaluate.settId(UUIDUtil.getUUID());
         tEvaluate.setCreateTime(dateNow);
-        tEvaluate.setfId(informationByTid.getpId());
+
         i = tEvaluateMapper.insertSelective(tEvaluate);
         if(i > 0){
             // 修改评论条数
             tInformation.settEvaluate(informationByTid.gettEvaluate()+1);
             i = tInformationMapper.updateByPrimaryKeySelective(tInformation);
         }
+        return i;
+    }
+
+    private int gettStore(TEvaluate tEvaluate) {
+        int i;
+        String dateNow = DateUtil.formatDate(new Date(),"yyyy-MM-dd HH:mm");
+        TStore tStore = tStoreMapper.selectByPrimaryKey(tEvaluate.gettOtherId());
+        tEvaluate.setfId(tStore.gettIssuer());
+        tEvaluate.settId(UUIDUtil.getUUID());
+        tEvaluate.setCreateTime(dateNow);
+        i = tEvaluateMapper.insertSelective(tEvaluate);
         return i;
     }
 
