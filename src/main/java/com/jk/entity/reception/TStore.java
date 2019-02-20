0b06d0ffa9430ee77e86eb3218e181e06fa440e4
kaixuan1314 <1197827508@qq,com>
2019-02-20 10:57:47 +0800

    商铺地址导航

--- src/main/java/com/jk/entity/reception/TStore.java
+++ src/main/java/com/jk/entity/reception/TStore.java
@@ -39,6 +39,10 @@ public class TStore extends Page {
     private String city;
     private String district;
 
+    private String longitude;
+
+    private String latitude;
+
     public String gettId() {
         return tId;
     }
@@ -197,5 +201,21 @@ public class TStore extends Page {
 
     public void setDistrict(String district) {
         this.district = district;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
     }
 }
